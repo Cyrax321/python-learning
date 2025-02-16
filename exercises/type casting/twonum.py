11b09d422e24e555eb6076be95f4d8aedf05caae
Cyrax321 <cyrax8590@gmail.com>
2025-02-16 09:11:52 +0530

    add Python scripts for basic arithmetic operations and type casting

---
+++ exercises/type casting/twonum.py
@@ -0,0 +1,12 @@
+num=input("enter a two digit number ")
+a=int(num[0])
+b=int(num[1])
+sum=a+b
+print(sum)
+
+# Output:
+#enter a two digit number 45
+#9
+
+#enter a two digit number 67
+#13
---
+++ operators/po.py
@@ -0,0 +1,6 @@
+a,b,c = 3,5,6
+print(a,b,c)
+c= a+b # addition of a and b 5+5 = 10 #8
+a += 2 # a = a+2 3+2 = 5 #5
+c += a # c = c+a 10+5 = 15 
+print (c)
